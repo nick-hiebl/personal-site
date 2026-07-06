96b9e151d862e5f318599591bdadaca32f6cd8e6
Nicholas Hiebl <nicholas@hiebl.net.au>
2026-07-06 19:20:52 +1000

    Rename another file to fix imports in builds

--- src/content/games/number-match/game/views/CompleteGame.tsx
+++ src/content/games/number-match/game/views/CompleteGame.tsx
@@ -1,5 +1,5 @@
 import { Grid } from '../components/Grid'
-import { Items } from '../components/Items'
+import { Items } from '../components/Items/index'
 import { PlayerList } from '../components/PlayerList'
 import { ValueDetails } from '../components/ValueDetails'
 import { useGameContext } from '../context'
